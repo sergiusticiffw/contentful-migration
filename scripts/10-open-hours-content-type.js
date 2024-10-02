6c2d1f81f06778309e2f61262c520d12924cdd85
Sergiu Stici <sergiu.stici@ffw.com>
2024-10-02 17:14:03 +0300

    CHILDRENS-11: Locations: Open hours

---
+++ scripts/10-open-hours-content-type.js
@@ -0,0 +1,182 @@
+module.exports = function (migration) {
+  // create new content type
+  const openHours = migration
+    .createContentType('openHours')
+    .name('Open Hours')
+    .description('');
+
+  // add field
+  openHours.createField('adminTitle', {
+    name: 'Admin title',
+    type: 'Symbol',
+    required: true,
+    validations: [
+      {
+        size: {
+          min: null,
+          max: 255,
+        }
+      }
+    ]
+  });
+  openHours.changeFieldControl('adminTitle', 'builtin', 'singleLine', {
+    helpText: 'Enter a unique ID for this content.',
+  });
+
+  // add entry title
+  openHours.displayField('adminTitle');
+
+  // add field
+  openHours.createField('sunday', {
+    name: 'Sunday',
+    type: 'Array',
+    required: false,
+    items: {
+      type: 'Link',
+      linkType: 'Entry',
+      validations: [
+        {
+          linkContentType: ['openClose'],
+        },
+      ],
+    },
+  });
+  openHours.changeFieldControl('sunday', 'builtin', 'entryLinksEditor', {
+    helpText: 'Indicate the opening and closing time(s) on this day. If left empty, location will be shown as closed on this day.',
+    bulkEditing: false,
+    showLinkEntityAction: true,
+    showCreateEntityAction: true
+  });
+
+  // add field
+  openHours.createField('monday', {
+    name: 'Monday',
+    type: 'Array',
+    required: false,
+    items: {
+      type: 'Link',
+      linkType: 'Entry',
+      validations: [
+        {
+          linkContentType: ['openClose'],
+        },
+      ],
+    },
+  });
+  openHours.changeFieldControl('monday', 'builtin', 'entryLinksEditor', {
+    helpText: 'Indicate the opening and closing time(s) on this day. If left empty, location will be shown as closed on this day.',
+    bulkEditing: false,
+    showLinkEntityAction: true,
+    showCreateEntityAction: true
+  });
+
+  // add field
+  openHours.createField('tuesday', {
+    name: 'Tuesday',
+    type: 'Array',
+    required: false,
+    items: {
+      type: 'Link',
+      linkType: 'Entry',
+      validations: [
+        {
+          linkContentType: ['openClose'],
+        },
+      ],
+    },
+  });
+  openHours.changeFieldControl('tuesday', 'builtin', 'entryLinksEditor', {
+    helpText: 'Indicate the opening and closing time(s) on this day. If left empty, location will be shown as closed on this day.',
+    bulkEditing: false,
+    showLinkEntityAction: true,
+    showCreateEntityAction: true
+  });
+
+  // add field
+  openHours.createField('wednesday', {
+    name: 'Wednesday',
+    type: 'Array',
+    required: false,
+    items: {
+      type: 'Link',
+      linkType: 'Entry',
+      validations: [
+        {
+          linkContentType: ['openClose'],
+        },
+      ],
+    },
+  });
+  openHours.changeFieldControl('wednesday', 'builtin', 'entryLinksEditor', {
+    helpText: 'Indicate the opening and closing time(s) on this day. If left empty, location will be shown as closed on this day.',
+    bulkEditing: false,
+    showLinkEntityAction: true,
+    showCreateEntityAction: true
+  });
+
+  // add field
+  openHours.createField('thursday', {
+    name: 'Thursday',
+    type: 'Array',
+    required: false,
+    items: {
+      type: 'Link',
+      linkType: 'Entry',
+      validations: [
+        {
+          linkContentType: ['openClose'],
+        },
+      ],
+    },
+  });
+  openHours.changeFieldControl('thursday', 'builtin', 'entryLinksEditor', {
+    helpText: 'Indicate the opening and closing time(s) on this day. If left empty, location will be shown as closed on this day.',
+    bulkEditing: false,
+    showLinkEntityAction: true,
+    showCreateEntityAction: true
+  });
+
+  // add field
+  openHours.createField('friday', {
+    name: 'Friday',
+    type: 'Array',
+    required: false,
+    items: {
+      type: 'Link',
+      linkType: 'Entry',
+      validations: [
+        {
+          linkContentType: ['openClose'],
+        },
+      ],
+    },
+  });
+  openHours.changeFieldControl('friday', 'builtin', 'entryLinksEditor', {
+    helpText: 'Indicate the opening and closing time(s) on this day. If left empty, location will be shown as closed on this day.',
+    bulkEditing: false,
+    showLinkEntityAction: true,
+    showCreateEntityAction: true
+  });
+
+  // add field
+  openHours.createField('saturday', {
+    name: 'Saturday',
+    type: 'Array',
+    required: false,
+    items: {
+      type: 'Link',
+      linkType: 'Entry',
+      validations: [
+        {
+          linkContentType: ['openClose'],
+        },
+      ],
+    },
+  });
+  openHours.changeFieldControl('saturday', 'builtin', 'entryLinksEditor', {
+    helpText: 'Indicate the opening and closing time(s) on this day. If left empty, location will be shown as closed on this day.',
+    bulkEditing: false,
+    showLinkEntityAction: true,
+    showCreateEntityAction: true
+  });
+};
